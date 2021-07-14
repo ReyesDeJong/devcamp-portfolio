6f31b7168cef6eea613c593c657d2c49bf0f1bdc
reyesdejong <reyes.dejong@gmail.com>
2021-07-14 00:15:34 -0400

    logout, dinamyc login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout',
-                                             sign_up: 'logout' }
+                                             sign_up: 'register' }
   resources :portfolios, except: [:show]
 
   get 'portfolios/angular', to: 'portfolios#angular'
